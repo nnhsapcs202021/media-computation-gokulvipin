464d87ef454433bc31cda1502d078d7396e226c9
Gokul vipin <gvipin@stu.naperville203.org>
2021-03-03 10:38:51 -0600

    New college class

---
+++ Collage.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import java.awt.font.*;
+import java.awt.geom.*;
+import java.awt.image.BufferedImage;
+import java.text.*;
+import java.util.*;
+import java.util.List;
+/**
+ * Write a description of class Collage here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Collage
+{
+    public static void main (){
+        
+    }
+     /*
+    public void cropAndCopy( Picture sourcePicture, int startSourceRow, int endSourceRow, int startSourceCol, int endSourceCol,
+         int startDestRow, int startDestCol ){
+             Pixel[][] sourcePixel = sourcePicture.getPixels2D();
+             Pixel[][] destPixel = this.getPixels2D();
+             
+             int rowRange = startDestRow - startSourceRow;
+             int colRange = startDestCol - startSourceCol;
+             
+             for (int row= startSourceRow; row<endSourceRow; row++){
+                 for (int col= startSourceCol; col<endSourceCol; col++){
+                     destPixel[row+rowRange][col+colRange].setColor(sourcePixel[row][col].getColor());
+                    }
+                }
+            }
+            */
+}
