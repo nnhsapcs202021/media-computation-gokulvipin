73aa575d0fb0996da8ccc891ea9b8a7b361d1143
Gokul vipin <gvipin@stu.naperville203.org>
2021-03-05 22:11:55 -0600

    Comments added to Collage lab.

--- Collage.java
+++ Collage.java
@@ -8,25 +8,32 @@ import java.util.List;
 /**
  * Write a description of class Collage here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author gokulvipin
+ * @version 3/5/2021
+ * A class to create a collage of 4 images
  */
 public class Collage
 {
     public static void main (String[] args){
+        
         Picture collage = new Picture(1000,6000);
         Picture messi1 = new Picture("messi.jpg");
         Picture messi2 = new Picture("messi.jpg");
         Picture messi3 = new Picture("messi.jpg");
         Picture messi4 = new Picture("messi.jpg");
-
+        
+        //One modification made to each image
         messi2.mirrorVerticalRightToLeft();
         messi3.greyscale();
         messi4.keepOnlyBlue();
-        // source image
+        
+        //original image
         collage.cropAndCopy(messi1,0,300,0,900,0,0);
+        //right and left switched
         collage.cropAndCopy(messi2,0,300,0,900,0,901);
+        //Grey image
         collage.cropAndCopy(messi3,0,300,0,900,301,0);
+        //Only blue image
         collage.cropAndCopy(messi4,0,300,0,900,301,901);
         
         collage.explore();
